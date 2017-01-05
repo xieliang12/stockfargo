f9a3c2e85373d36feeb7f160abc0769a1f88bef6
Liang Xie <xieliang12@gmail.com>
2017-01-04 18:22:49 -0700

    figured out using local storage saving the table display style for page refreshing

--- vendor/assets/javascripts/stocks_table_display.js
+++ vendor/assets/javascripts/stocks_table_display.js
@@ -1,5 +1,49 @@
-$(document).ready(function() {
-  $('#stocks').dataTable( {
-   "bFilter": false
-  } );
-} );
+var Ctable;
+$('#overview').click(function() {
+  document.getElementById("overview").style.background='#808080';
+  document.getElementById("valuation").style.background='#DCDCDC';
+  document.getElementById("financial").style.background='#DCDCDC';
+  $('#stocks2').hide();
+  $('#stocks3').hide();
+  $('#stocks1').show();
+  Ctable = "table1";
+  localStorage.setItem('Ctable', Ctable);
+});
+$('#valuation').click(function() {
+  document.getElementById("overview").style.background='#DCDCDC';
+  document.getElementById("valuation").style.background='#808080';
+  document.getElementById("financial").style.background='#DCDCDC';
+  $('#stocks1').hide();
+  $('#stocks3').hide();
+  $('#stocks2').show();
+  Ctable = "table2";
+  localStorage.setItem('Ctable', Ctable);
+});
+$('#financial').click(function() {
+  document.getElementById("overview").style.background='#DCDCDC';
+  document.getElementById("valuation").style.background='#DCDCDC';
+  document.getElementById("financial").style.background='#808080';
+  $('#stocks1').hide();
+  $('#stocks2').hide();
+  $('#stocks3').show();
+  Ctable = "table3";
+  localStorage.setItem('Ctable', Ctable);
+});
+
+var table_id = localStorage.getItem('Ctable');
+if (table_id == "table2") {
+  document.getElementById("overview").style.background='#DCDCDC';
+  document.getElementById("valuation").style.background='#808080';
+  document.getElementById("financial").style.background='#DCDCDC';
+  $('#stocks2').show();
+} else if (table_id == "table3") {
+  document.getElementById("overview").style.background='#DCDCDC';
+  document.getElementById("valuation").style.background='#DCDCDC';
+  document.getElementById("financial").style.background='#808080';
+  $('#stocks3').show();
+} else {
+  document.getElementById("overview").style.background='#808080';
+  document.getElementById("valuation").style.background='#DCDCDC';
+  document.getElementById("financial").style.background='#DCDCDC';
+  $('#stocks1').show();
+}
